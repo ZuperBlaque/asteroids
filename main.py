c143565d5debfe0c765d9a28ac357a068e7d2f5d
ZuperBlaque <rikijuma23@gmail.com>
2025-08-29 17:17:17 +1000

    Created sprite groups

--- main.py
+++ main.py
@@ -5,27 +5,31 @@ import pygame
 from constants import *
 from player import Player
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    
+    # Create sprite groups for all updatable and drawable objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)    # Set the containers for the Player class
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")  # Fill the screen with black
-        player.update(dt)      # Update the player each frame
-        player.draw(screen)   # Draw the player each frame
+        updatable.update(dt)      # Update the player each frame
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()   # Refresh the screen
         dt = clock.tick(60) / 1000  # Amount of seconds between each loop
 
