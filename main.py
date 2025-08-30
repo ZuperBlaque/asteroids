69d5852bdf6c3f6a9927d923249b35bbf355242c
ZuperBlaque <rikijuma23@gmail.com>
2025-08-30 14:45:29 +1000

    Asteroid field

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -18,8 +20,14 @@ def main():
     # Create sprite groups for all updatable and drawable objects
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)    # Set the containers for the Player class
+    Asteroid.containers = (asteroids, updatable, drawable)  # Set the containers for the Asteroid class
+    AsteroidField.containers = (updatable,)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     dt = 0
     while True:
